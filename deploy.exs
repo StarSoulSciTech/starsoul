3154393f28e00cd95c4ab394fc4572c7bc16aa28
loongmxbt <loongmxbt@gmail.com>
2016-11-24 17:35:59 +0800

    deploy

--- deploy.exs
+++ deploy.exs
@@ -4,7 +4,7 @@ defmodule Starsoul.DeployCallbacks do
   def after_mix_digest(env) do
     bash("mkdir", ~w[-p priv/static], cd: env.build_dir) # optional: release may complain about this directory not existing
     bash("npm", ~w[install], cd: env.build_dir)
-    bash("./node_modules/brunch/bin/brunch", ~w[build --production], cd: env.build_dir)
+    bash("brunch", ~w[build --production], cd: env.build_dir)
     bash("mix", ~w[phoenix.digest -o public/static], cd: env.build_dir)
     env
   end
