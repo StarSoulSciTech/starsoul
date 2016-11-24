e9f0442fdc9601e61022df99fd57b44ee2fa1315
loongmxbt <loongmxbt@gmail.com>
2016-11-24 20:57:30 +0800

    upgrade

--- upgrade.exs
+++ upgrade.exs
@@ -1,11 +1,11 @@
 defmodule Starsoul.UpgradeCallbacks do
   import Gatling.Bash
 
-  def after_mix_digest(env) do
-    bash("npm", ~w[install], cd: env.build_dir)
-    bash("brunch", ~w[build --production], cd: env.build_dir)
-    bash("mix", ~w[phoenix.digest -o public/static], cd: env.build_dir)
-    env
-  end
+  # def after_mix_digest(env) do
+  #   bash("npm", ~w[install], cd: env.build_dir)
+  #   bash("brunch", ~w[build --production], cd: env.build_dir)
+  #   bash("mix", ~w[phoenix.digest -o public/static], cd: env.build_dir)
+  #   env
+  # end
 
 end
